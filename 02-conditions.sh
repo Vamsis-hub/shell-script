43c6536e24bae950e21c952a9bb3cb4d7163608e
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-15 08:56:10 +1000

    changes

--- 02-conditions.sh
+++ 02-conditions.sh
@@ -6,11 +6,26 @@ ID=$(id -u)
 if [ $ID -ne 0 ] 
 then
    echo "Error,please run with root user"
-   exit 1
 else
    echo "you are a root user"
 fi   
 
 yum install mysql -y
 
+if [ $? -ne 0 ]
+then 
+   echo "Error :MYSQL INSTALLATION FAILED"
+   exit 1
+else
+   echo "MYSQL SUCCESS"
+fi      
+
 yum install git -y
+
+if [ $? -ne 0 ]
+then 
+   echo "Error :GIT INSTALLATION FAILED"
+   exit 1
+else
+   echo "GIT SUCCESS"
+fi      
