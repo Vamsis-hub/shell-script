c8d22fe872cdc108049f103c648b3c49f1fa4ea9
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-15 08:26:18 +1000

    changes

--- 02-conditions.sh
+++ 02-conditions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ID=(ID -u)
+ID=$(ID -u)
 
 
-if ($ID -ne 0)
-   then
-   echo:"Error,please run with root user"
-   else
-   echo:"you are a root user"
+if ( $ID -ne 0 )
+then
+   echo "Error,please run with root user"
+else
+   echo "you are a root user"
 fi   
