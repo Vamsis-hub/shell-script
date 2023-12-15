6f030119c6dc4cdcfa570b5ce58161112b12adae
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-15 11:42:41 +1000

    changes

---
+++ 03-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+scriptname = $0
+ID=$(id -u)
+
+Variable() {   
+      if [ $1 -ne 0 ]
+then 
+   echo "$2 Installation FAILED"
+else
+   echo "$2 Installation SUCESS"
+fi   
+
+
+}
+
+if [ $ID -ne 0 ] 
+then
+   echo "Error,please run with root user"
+   exit 1
+else
+   echo "you are a root user"
+fi   
+
+yum install mysql -y
+ 
+Variable $? Mysql
+
+yum install git -y
+
+Variable $? Git
