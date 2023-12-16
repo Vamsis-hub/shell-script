d754900803f29f1969006495e008a7fe88070da3
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-16 16:56:02 +1000

    changes

--- 05-installpackages.sh
+++ 05-installpackages.sh
@@ -13,10 +13,10 @@ Logfile="/tmp/$0-$TIME.log"
 validate(){ 
   if [ $1 -ne 0 ] 
     then
-       echo -e "$2 ... $R failed $N" &>> $Logfile
+       echo -e "$2 ... $R failed $N"
        exit 1
     else
-       echo -e "$2...  $R success $N" &>> $Logfile
+       echo -e "$2...  $R success $N" 
   fi     
 
 }
