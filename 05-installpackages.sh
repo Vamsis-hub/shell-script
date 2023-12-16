71e109bc0408cc4fcb27b3663e058b17dac82023
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-16 17:03:46 +1000

    changes

--- 05-installpackages.sh
+++ 05-installpackages.sh
@@ -23,7 +23,7 @@ validate(){
 
 if [ $ID -ne 0 ]
 then
-   echo -e "$R Error,please run with root user $N" 
+   echo -e "$R Error,please run with root user $N"  &>> $Logfile
    exit 1
 else
    echo "I am root user" &>> $Logfile
