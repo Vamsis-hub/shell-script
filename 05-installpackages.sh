4375e448dc6203dd5ea9d3f2826dd674d529f37b
vamsis-hub <vamsireddychaganti.19@gmail.com>
2023-12-16 16:51:16 +1000

    changes

--- 05-installpackages.sh
+++ 05-installpackages.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIME=$(date +%F-%H-%M-%S)
+
+Logfile="/tmp/$0-$TIME.log"
+
+validate(){ 
+  if [ $1 -ne 0 ] 
+    then
+       echo -e "$2 ... $R failed $N" &>> $Logfile
+       exit 1
+    else
+       echo -e "$2...  $R success $N" &>> $Logfile
+  fi     
+
+}
 
 if [ $ID -ne 0 ]
 then
-   echo "Error,please run with root user"
+   echo -e "$R Error,please run with root user $N" &>> $Logfile
    exit 1
 else
-   echo "I am root user"
+   echo "I am root user" &>> $Logfile
 fi 
 
 
 for package in $@
 do
-    yum installed is listed $package
+    yum installed is listed $package &>> $Logfile
   if [ $? -ne 0 ]
   then
-     yum install $package -y
- # validate $?,installation of $package
+     yum install $package -y  &>> $Logfile
+     validate $?,installation of $package
   else
      echo "if the $package is already installed ... Skip"
   fi
